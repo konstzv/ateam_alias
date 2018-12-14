3c0dd70e1b8af410571848fd9e882cea4e08bd75
Konstantin Zagulin <konstzv@gmail.com>
2018-12-15 00:01:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/androidacademy/ateam/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.androidacademy.ateam
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
