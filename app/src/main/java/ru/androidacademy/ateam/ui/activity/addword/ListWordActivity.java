bcbd6c47315ee209259a03d195f416503eed32b5
Konstantin Zagulin <konstzv@gmail.com>
2018-12-18 23:08:58 +0300

    Commented out parts for refactor

--- app/src/main/java/ru/androidacademy/ateam/ui/activity/addword/ListWordActivity.java
+++ app/src/main/java/ru/androidacademy/ateam/ui/activity/addword/ListWordActivity.java
@@ -25,51 +25,51 @@ import java.util.List;
 import java.util.Objects;
 
 public class ListWordActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    DecksChooseListAdapter decksChooseListAdapter;
-    AppDataBase db = App.getInstance().getDbInstance();
-    DeckDao deckDao = db.deckDao();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_word);
-        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.bar_word_list);
-
-        recyclerView = findViewById(R.id.add_word_activity_recycler);
-        initRecyclerView(recyclerView, this);
-        decksChooseListAdapter.getOnDeckItemClickPublishSubject().subscribe(
-                new Consumer<Deck>() {
-                    @Override
-                    public void accept(Deck deck) throws Exception {
-                        ListWordInDeckFragment listWordInDeckFragment = ListWordInDeckFragment.newInstance(deck.deckId, deck.deckName);
-                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                        ft.add(R.id.fragment, listWordInDeckFragment);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                    }
-                }
-        );
-        deckDao.getAllDeck()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new BiConsumer<List<Deck>, Throwable>() {
-                    @Override
-                    public void accept(List<Deck> decks, Throwable throwable) throws Exception {
-                        ArrayList<Deck> list = new ArrayList<>();
-                        list.addAll(decks);
-                        decksChooseListAdapter.setItems(list);
-                        decksChooseListAdapter.notifyDataSetChanged();
-                    }
-                });
-
-    }
-
-
-    private void initRecyclerView(@NonNull RecyclerView recyclerView, @NonNull Context context) {
-        decksChooseListAdapter = new DecksChooseListAdapter();
-        recyclerView.addItemDecoration(new ItemOffsetDecoration(this, R.dimen.activity_padding_8dp));
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        recyclerView.setAdapter(decksChooseListAdapter);
-    }
+//    RecyclerView recyclerView;
+//    DecksChooseListAdapter decksChooseListAdapter;
+//    AppDataBase db = App.getInstance().getDbInstance();
+//    DeckDao deckDao = db.deckDao();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list_word);
+//        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.bar_word_list);
+//
+//        recyclerView = findViewById(R.id.add_word_activity_recycler);
+//        initRecyclerView(recyclerView, this);
+//        decksChooseListAdapter.getOnDeckItemClickPublishSubject().subscribe(
+//                new Consumer<Deck>() {
+//                    @Override
+//                    public void accept(Deck deck) throws Exception {
+//                        ListWordInDeckFragment listWordInDeckFragment = ListWordInDeckFragment.newInstance(deck.deckId, deck.deckName);
+//                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//                        ft.add(R.id.fragment, listWordInDeckFragment);
+//                        ft.addToBackStack(null);
+//                        ft.commit();
+//                    }
+//                }
+//        );
+//        deckDao.getAllDeck()
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new BiConsumer<List<Deck>, Throwable>() {
+//                    @Override
+//                    public void accept(List<Deck> decks, Throwable throwable) throws Exception {
+//                        ArrayList<Deck> list = new ArrayList<>();
+//                        list.addAll(decks);
+//                        decksChooseListAdapter.setItems(list);
+//                        decksChooseListAdapter.notifyDataSetChanged();
+//                    }
+//                });
+//
+//    }
+//
+//
+//    private void initRecyclerView(@NonNull RecyclerView recyclerView, @NonNull Context context) {
+//        decksChooseListAdapter = new DecksChooseListAdapter();
+//        recyclerView.addItemDecoration(new ItemOffsetDecoration(this, R.dimen.activity_padding_8dp));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+//        recyclerView.setAdapter(decksChooseListAdapter);
+//    }
 }
--- app/src/main/java/ru/androidacademy/ateam/ui/activity/addword/WordAddToDeck.java
+++ app/src/main/java/ru/androidacademy/ateam/ui/activity/addword/WordAddToDeck.java
@@ -18,59 +18,54 @@ import ru.androidacademy.ateam.model.dao.WordDao;
 import ru.androidacademy.ateam.model.tables.Deck;
 import ru.androidacademy.ateam.model.tables.Word;
 
-import javax.security.auth.callback.Callback;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.Callable;
 
 public class WordAddToDeck extends AppCompatActivity {
-    Button btnSave;
-    EditText editText;
-    String temp;
-    AppDataBase db = App.getDbInstance();
-    WordDao wordDao = db.wordDao();
-    Word word = new Word();
-    DeckDao deckDao= db.deckDao();
-    Deck deck = new Deck();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_word_add_to_deck);
-        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.bar_add_words);
-
-        btnSave = findViewById(R.id.btn_save_word);
-        editText = findViewById(R.id.word_add_edit_text);
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                temp = editText.getText().toString();
-                Completable.fromAction(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        String[] s = checkString(temp);
-                        for (int i = 0; i< s.length; i++){
-                            word.deckId = 3;
-                            word.word = s[i];
-                            wordDao.insert(word);
-                        }
-                       deck.countWordInDeck = wordDao.getCountWordsById(3);
-                       deckDao.insert(deck);
-                    }
-                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe();
-               }
-        });
-
-    }
-    private String[] checkString(String temp) {
-        String[] tempStr = temp.split(",");
-        for (int i = 0; i < tempStr.length; i++) {
-            tempStr[i] = tempStr[i].trim();
-            Log.d("Taf", tempStr[i]);
-        }
-        return tempStr;
-    }
+//    Button btnSave;
+//    EditText editText;
+//    String temp;
+//    AppDataBase db = App.getDbInstance();
+//    WordDao wordDao = db.wordDao();
+//    Word word = new Word();
+//    DeckDao deckDao= db.deckDao();
+//    Deck deck = new Deck();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_word_add_to_deck);
+//        Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.bar_add_words);
+//
+//        btnSave = findViewById(R.id.btn_save_word);
+//        editText = findViewById(R.id.word_add_edit_text);
+//
+//        btnSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                temp = editText.getText().toString();
+//                Completable.fromAction(new Action() {
+//                    @Override
+//                    public void run() throws Exception {
+//                        String[] s = checkString(temp);
+//                        for (int i = 0; i< s.length; i++){
+//                            word.deckId = 3;
+//                            word.text = s[i];
+//                            wordDao.insert(word);
+//                        }
+//                       deck.countWordInDeck = wordDao.getCountWordsById(3);
+//                       deckDao.insert(deck);
+//                    }
+//                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe();
+//               }
+//        });
+//
+//    }
+//    private String[] checkString(String temp) {
+//        String[] tempStr = temp.split(",");
+//        for (int i = 0; i < tempStr.length; i++) {
+//            tempStr[i] = tempStr[i].trim();
+//            Log.d("Taf", tempStr[i]);
+//        }
+//        return tempStr;
+//    }
 }
