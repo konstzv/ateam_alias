2c3b4c8f9ccc22be5108e2440c8834d38a41b8fc
Konstantin Zagulin <konstzv@gmail.com>
2019-01-16 11:09:41 +0300

    remove choose random player on init

--- app/src/main/java/ru/androidacademy/ateam/model/Team.kt
+++ app/src/main/java/ru/androidacademy/ateam/model/Team.kt
@@ -5,13 +5,13 @@ import kotlin.random.Random
 
 data class Team(val name: String, val players: List<Player>, var score: Int = 0) {
 
-    var index: Int
+    var index: Int = 0
     lateinit var currentPlayer: Player
 
-    init {
-        index = Random.nextInt(0, players.size)
-        getNextPlayer()
-    }
+//    init {
+//        index = Random.nextInt(0, players.size)
+//        getNextPlayer()
+//    }
 
     fun getNextPlayer(): Player {
         val player = players[nextIndex()]
